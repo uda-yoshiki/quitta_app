4a80923ec7ce60300ceba583e96d5d2fa0a1bf58
uda-yoshiki <uda-yoshiki@it-edu-suport.com>
2024-02-13 23:52:14 +0900

    20240213実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:qiita_app/screens/search_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // packageをインポート
 
-void main() {
+// main関数をFutureに変更
+Future<void> main() async {
+  await dotenv.load(fileName: '.env'); // .envファイルを読み込み
   runApp(const MainApp());
 }
 
@@ -9,12 +13,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Qiita Search',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        fontFamily: 'Hiragino Sans',
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF55C500),
         ),
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white,
+            ),
       ),
+      home: const SearchScreen(),
     );
   }
 }
